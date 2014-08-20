3623a7309b40c292fb9d548e772e66e8c94cec5c
kaiohken1982@hotmail.com <kaiohken1982@hotmail.com>
2014-08-20 03:23:40 +0200

    Added comments

--- src/CardTest/Game.php
+++ src/CardTest/Game.php
@@ -4,49 +4,101 @@ namespace CardTest;
 use \CardTest\Game\Player\Player,
     \CardTest\Game\Deck\Deck;
 
+/**
+ * The game class wraps all the players and a deck of cards
+ * 
+ * @author Sergio
+ */
 class Game 
 {
-  protected $players; 
+    /**
+     * @var array
+     */
+    protected $players; 
+    
+    /**
+     * @var Deck
+     */
+    protected $deck; 
 
-  protected $deck; 
+    /**
+     * @var Int
+     */
+    const PLAYER_CARDS = 7;
 
-  const PLAYER_CARDS = 7;
+    /**
+     * Adds a player object to the game
+     * 
+     * @param Player $player
+     * @return \CardTest\Game
+     */
+    public function addPlayer(Player $player)
+    {
+        $this->players[] = $player;
+        
+        return $this;
+    }
 
-  public function addPlayer(Player $player) 
-  {
-    $this->players[] = $player;
-  } 
+    /**
+     * Get game players
+     * 
+     * @return array
+     */
+    public function getPlayers()
+    {
+        return $this->players;
+    }
 
-  public function getPlayers() 
-  {
-    return $this->players; 
-  }
+    /**
+     * Set a deck for the game
+     * 
+     * @param Deck $deck
+     * @return \CardTest\Game
+     */
+    public function setDeck(Deck $deck)
+    {
+        $this->deck = $deck;
 
-  public function setDeck(Deck $deck) 
-	{
-    $this->deck = $deck; 
+        return $this;
+    }
 
-    return $this;
-  } 
+    /**
+     * Get the deck for this game
+     * 
+     * @return \CardTest\Game\Deck\Deck
+     */
+    public function getDeck()
+    {
+        return $this->deck;
+    }
+    
+    /**
+     * Deal card for each player
+     * 
+     * @return \CardTest\Game
+     */
+    public function dealCards()
+    {
+        $players = $this->getPlayers();
+        for($i = 0; $i < self::PLAYER_CARDS; $i++) {
+            foreach($players as $player) {
+                $this->getDeck()->dealCard($player);
+            }
+        }
 
-  public function getDeck() 
-  {
-    return $this->deck;
-  } 
+        return $this;
+    }
 
-  public function dealCards() 
-  {
-	  $players = $this->getPlayers();
-	  for($i = 0; $i < self::PLAYER_CARDS; $i++) {
-	    foreach($players as $player) {
-	      $this->getDeck()->dealCard($player);
-      }
-	  }
-  }
+    /**
+     * Set up the game
+     * 
+     * @return \CardTest\Game
+     */
+    public function setUp()
+    {
+        $this->getDeck()->shuffle();
+        $this->dealCards();
 
-  public function setUp()
-  {
-	  $this->getDeck()->shuffle();
-	  $this->dealCards();
-  }
+        return $this;
+    }
 }
